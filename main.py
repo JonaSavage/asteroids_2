7e92498f149b13c2dec23cb07f347aca5c9ac442
JonaSavage <jonathan.stickle@gmail.com>
2025-09-12 11:13:57 -0700

    Completed 2-2. Making Screen appear and exiting correctly

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
+# Disable "Hello from PyGame COmmunity" message
 from os import environ
 environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
+# ---------------------------------------
 import pygame
+from constants import *
 
 
 def main():
+    pygame.init()
+    
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    while True:
+        # Quits if user clicks exit window button
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+    
+        pygame.Surface.fill(screen, "black")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
