9417397fc5066c6d491cb1c93a7944b89c6d1d1a
JonaSavage <jonathan.stickle@gmail.com>
2025-09-15 12:07:23 -0700

    Completed Chapter 2: Player.

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
-# Disable "Hello from PyGame COmmunity" message
+# Disable "Hello from PyGame Community" message
 from os import environ
 environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "hide"
+
+# Run this in order to run the program:
+# uv run main.py
 # ---------------------------------------
 import pygame
 from constants import *
+from player import *
 
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     
     print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    # print(f"Screen width: {SCREEN_WIDTH}")
+    # print(f"Screen height: {SCREEN_HEIGHT}")
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     while True:
         # Quits if user clicks exit window button
@@ -21,8 +33,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
     
-        pygame.Surface.fill(screen, "black")
+        screen.fill("black")
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
+        
+        dt = clock.tick(60) / 1000
+        
 
 
 if __name__ == "__main__":
